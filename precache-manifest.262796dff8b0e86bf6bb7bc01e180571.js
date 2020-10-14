ba7b2aeb2df40423dff1b6b57b8fd6240642ff6a
MariyaLcs <mariya.podosinova@gmail.com>
2020-10-14 14:22:05 +0100

    Updates

---
+++ precache-manifest.262796dff8b0e86bf6bb7bc01e180571.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaff2e7c0906863a4fdb07df33badd1f",
+    "url": "/songs-react-redux/index.html"
+  },
+  {
+    "revision": "c362fa9063c46a781dad",
+    "url": "/songs-react-redux/static/js/2.70705a15.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/songs-react-redux/static/js/2.70705a15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62880d2993b31b123833",
+    "url": "/songs-react-redux/static/js/main.3d99707b.chunk.js"
+  },
+  {
+    "revision": "dbff2080d55618103e3e",
+    "url": "/songs-react-redux/static/js/runtime-main.cd99cd4d.js"
+  }
+]);
